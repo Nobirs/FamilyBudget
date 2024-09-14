edeb99f82252f2171d11aa48fa886f3df3285360
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-09-14 21:28:03 +0300

    Fix GetNonExistingUserTest which FAILED before

--- tests/UserTests.cpp
+++ tests/UserTests.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 #include "User.h"
+#include "UserRepository.h"
 
 TEST(UserTest, AuthenticateUser) {
     User user("testuser", "password", UserRole::USER);
+
     ASSERT_TRUE(user.authenticate("password"));
     ASSERT_FALSE(user.authenticate("wrongpassword"));
 }
@@ -10,9 +12,63 @@ TEST(UserTest, AuthenticateUser) {
 TEST(UserTest, ChangePassword) {
     User user("testuser", "password", UserRole::USER);
     user.changePassword("newpassword");
+
     ASSERT_TRUE(user.authenticate("newpassword"));
     ASSERT_FALSE(user.authenticate("password"));
 }
+
+
+class UserRepositoryTests: public ::testing::Test {
+protected:
+    UserRepository userRepository;
+
+    void TearDown() override {
+        userRepository.clearUsersTable();
+    }
+
+};
+
+
+TEST_F(UserRepositoryTests, AddUserTest) {
+    User newUser("testUser", "testPassword", UserRole::USER);
+    userRepository.addUser(newUser);
+    User fetchedUser = userRepository.getUserByUsername("testUser");
+    
+    EXPECT_EQ(fetchedUser.getUsername(), newUser.getUsername());
+    EXPECT_EQ(fetchedUser.getRole(), newUser.getRole());
+}
+
+TEST_F(UserRepositoryTests, GetUserByUsernameTest) {
+    User user("testUser", "testPassword", UserRole::USER);
+    userRepository.addUser(user);
+    User retrievedUser = userRepository.getUserByUsername("testUser");
+    
+    EXPECT_EQ(retrievedUser.getUsername(), "testUser");
+}
+
+TEST_F(UserRepositoryTests, GetAllUsersTest) {
+    userRepository.addUser(User("user1", "password1", UserRole::USER));
+    userRepository.addUser(User("user2", "password2", UserRole::USER));
+    vector<User> users = userRepository.getAllUsers();
+    
+    EXPECT_EQ(users.size(), 2);
+    EXPECT_EQ(users[0].getUsername(), "user1");
+    EXPECT_EQ(users[1].getUsername(), "user2");
+}
+
+TEST_F(UserRepositoryTests, GetNonExistingUserTest) {
+    EXPECT_THROW(userRepository.getUserByUsername("nonExistent"), std::runtime_error);
+}
+
+TEST_F(UserRepositoryTests, DuplicateUserTest) {    
+    User user("duplicateUser", "password123", UserRole::USER);
+    userRepository.addUser(user);
+    bool result = userRepository.addUser(user);  // Modify addUser to return a boolean if successful
+    
+    EXPECT_FALSE(result);
+}
+
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
